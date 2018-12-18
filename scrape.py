3c222091789767b3b34d4786253f6b7779d53629
Nathan Zheng <nyz3@cornell.edu>
2018-12-18 16:35:57 -0600

    installed a few dependencies

---
+++ scrape.py
@@ -0,0 +1,18 @@
+
+# dependencies/what I did
+
+# installed 'pip' for easy module installation ...  https://pip.pypa.io/en/stable/installing/
+
+# to install any dependencies with pip... py -m pip install <package>
+# 1. install requests
+# 2. install bs4 (comes with BeautifulSoup as subpackage)
+# 3. install pandas
+
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+print('Hello World!')
+
+
+
